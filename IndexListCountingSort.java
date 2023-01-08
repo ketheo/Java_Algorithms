a8db7613c21d4f1ea028c4c65fad8dc1f4501f7a
Karen Theodosopoulos <ketheo@gmail.com>
2023-01-08 12:11:10 -0500

    Seperating the output

--- IndexListCountingSort.java
+++ IndexListCountingSort.java
@@ -9,22 +9,22 @@ public class IndexListCountingSort {
             int num = (int)(Math.random()*100);
             arr.add(i, num);
             }
-        System.out.println(arr);
+        System.out.println("\n" + arr + "\n");
         Collections.sort(arr);
-        System.out.println(arr);
+        System.out.println(arr + "\n");
         ArrayList<Integer> indexList = new ArrayList<Integer>();
         
         // Create new list, length 100, filled with zeros.
         for(int i = 0; i < 100; i++){
             indexList.add(i, 0);}
-        System.out.println(indexList);
+        System.out.println(indexList + "\n");
 
         //
         for (int k = 0; k < arr.size(); k++){
             int indexNumber = arr.get(k);
             indexList.set(indexNumber, indexList.get(indexNumber) + 1);
         }
-        System.out.println(indexList);
+        System.out.println(indexList + "\n");
         }
     }
 
