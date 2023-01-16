5d2bb9525785b9cef0f8b8ce13ef92d36218939d
Karen Theodosopoulos <ketheo@gmail.com>
2023-01-16 10:31:46 -0500

    listingUpdate

--- IndexListCountingSort.java
+++ IndexListCountingSort.java
@@ -25,6 +25,31 @@ public class IndexListCountingSort {
             indexList.set(indexNumber, indexList.get(indexNumber) + 1);
         }
         System.out.println(indexList + "\n");
+        int matrix[][];
+        //matrix[0][0] = 4;
         }
     }
+
+    /*
+     * 
+    public static int flippingMatrix(List<List<Integer>> matrix) {
+    // Write your code here
+    int[] temp = new int[matrix.size()];
+    int col = 2;
+    int count = 0;
+    for(int r = matrix.size()-1; r >= 0; r--){
+        int tempNum = (matrix.get(r).get(col));
+        temp[count] = tempNum;
+        count ++;    
+        
+    }
+    for (int k = 0; k < temp.length; k++){
+        System.out.println(temp[k]);
+    }
+
+    return 7;
+    }
+
+}
+     */
 
